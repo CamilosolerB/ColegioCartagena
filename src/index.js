e7a0bc546c93a2cae21ff547710e3025ba9088d7
CamilosolerB <caansobu2@gmail.com>
2022-05-05 19:51:01 -0500

    buffaso

--- src/index.js
+++ src/index.js
@@ -2,19 +2,28 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const path = require('path');
+const ejs = require('ejs')
 
 //modo de desarrollo y uso de JSON
 app.use(morgan('dev'));
 app.use(express.json());
 
 //routers
-app.use(require('./desing.routes'))
+app.use(require('./routes/desing.routes'))
 app.use('/students',require('./routes/students.routes'))
 
+//uso de motores de platillas (en este caso se usa ejs)
+/* Telling the server to use the ejs engine to render the views. */
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,'public'));
+
+
 //uso de archivos estaticos
+/* Telling the server to use the public folder as the root folder for the server. */
 app.use(express.static(path.join(__dirname,'public')));
 app.use(express.static(path.join(__dirname,"static")));
 
+/* Setting the port to 3000 if there is no port set in the environment. */
 app.set('port', process.env.PORT || 3000);
 
 //configuracion del puerto
--- src/routes/desing.routes.js
+++ src/routes/desing.routes.js
@@ -3,7 +3,7 @@ const path = require('path');
 const router = express.Router();
 
 router.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname, 'public/index.html'))
+    res.render('index');
 })
 
 router.get('/nosotros', (req,res)=>{
