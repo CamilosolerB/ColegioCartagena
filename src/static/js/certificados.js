c2f89b4b4d2b11887f7d32c62b9ba4dbe842b9a5
CamilosolerB <caansobu2@gmail.com>
2022-06-20 15:33:20 -0500

    ya el video

--- src/static/js/certificados.js
+++ src/static/js/certificados.js
@@ -1,15 +1,34 @@
 function generarpago(){
-    fetch('/students/documento')
-    .then(response => response.json())
-    .then(data => {
-        Swal.fire({
-            icon: 'success',
-            title: 'certificado generado correctamente',
-            showConfirmButton: false,
-            timer: 1500
-          })
-    }
-    )
+    Swal.fire({
+        title: '¿Estas seguro de solicitar el certificado?',
+        showCancelButton: true,
+        confirmButtonText: 'Look up',
+        showLoaderOnConfirm: true,
+        preConfirm: (login) => {
+          return fetch('/students/documento')
+            .then(response => {
+              if (!response.ok) {
+                throw new Error(response.statusText)
+              }
+              return response.json()
+            })
+            .catch(error => {
+              Swal.showValidationMessage(
+                `Request failed: ${error}`
+              )
+            })
+        },
+        allowOutsideClick: () => !Swal.isLoading()
+      }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire({
+                icon: 'success',
+                title: 'certificado generado correctamente',
+                showConfirmButton: false,
+                timer: 1500
+              })
+        }
+      })
 }
 let boton = document.getElementById('boton');
 
