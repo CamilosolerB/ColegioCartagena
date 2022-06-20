990b19b2cd4dbfeac07a20a46c6619f9cfee3576
CamilosolerB <caansobu2@gmail.com>
2022-06-20 11:52:14 -0500

    certificado

--- src/static/js/certificados.js
+++ src/static/js/certificados.js
@@ -1,26 +1,13 @@
 function generarpago(){
-    let email = document.getElementById('correo').value;
-    let telefono = document.getElementById('telefono').value;
-    const data = {
-        email: email,
-        telefono: telefono
-    }
-    fetch('/students/certificados',{
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
+    fetch('/students/documento')
     .then(response => response.json())
     .then(data => {
         Swal.fire({
             icon: 'success',
-            title: data.message,
+            title: 'certificado generado correctamente',
             showConfirmButton: false,
             timer: 1500
           })
-          window.location.reload()
     }
     )
 }
